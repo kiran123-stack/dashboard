1bc2532b4cb49d585d532d79f32c9b4a4e640085
Kiran <kiranpar321@gmail.com>
2026-04-05 17:50:07 +0530

    Update AppContext.jsx

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,55 +1,43 @@
 import { createContext, useContext, useState } from "react";
 import { transactions as initialData } from "../data/mockData";
 
-// Create Context
 const AppContext = createContext();
 
-// Provider Component
 export const AppProvider = ({ children }) => {
   const [userRole, setUserRole] = useState("admin"); 
   const [transactions, setTransactions] = useState(initialData);
 
-  //  Add Transaction
   const addTransaction = (newTransaction) => {
     setTransactions((prev) => [
-      {
-        id: Date.now(),
-        ...newTransaction,
-      },
+      { id: Date.now(), ...newTransaction },
       ...prev,
     ]);
   };
 
+  const deleteTransaction = (id) => {
+    setTransactions((prev) => prev.filter((t) => t.id !== id));
+  };
+
+  const editTransaction = (updatedTransaction) => {
+    setTransactions((prev) =>
+      prev.map((t) => (t.id === updatedTransaction.id ? updatedTransaction : t))
+    );
+  };
+
   return (
     <AppContext.Provider
-   value={{
-    transactions,
-    addTransaction,
-    deleteTransaction,
-    editTransaction,
-    userRole,
-    setUserRole,
-  }}
->
+      value={{
+        transactions,
+        addTransaction,
+        deleteTransaction,
+        editTransaction,
+        userRole,
+        setUserRole,
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
 };
 
-// Custom Hook (easy access)
-export const useAppContext = () => {
-  return useContext(AppContext);
-};
-
-const deleteTransaction = (id) => {
-  setTransactions((prev) => prev.filter((t) => t.id !== id));
-};
-
-// Edit Transaction
-const editTransaction = (updatedTransaction) => {
-  setTransactions((prev) =>
-    prev.map((t) =>
-      t.id === updatedTransaction.id ? updatedTransaction : t
-    )
-  );
-};
+export const useAppContext = () => useContext(AppContext);
